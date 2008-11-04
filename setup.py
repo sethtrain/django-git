88e775b1b762bd962346d888854dc5e8f46cf900
Seth Buntin <seth.buntin@gmail.com>
2008-11-04 15:20:58 +0000

    add distutil install functionality and installation help file

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django_git',
+    version='0.1',
+    description='django gitweb replacement',
+    classifiers = [ 'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    author='Seth Buntin',
+    author_email='sethtrain@gmail.com',
+    url='http://code.google.com/p/django-git',
+    packages=['django_git'],
+    requires=['GitPython (>=0.1.4)', 'Pygments (>=0.11)'],
+)
