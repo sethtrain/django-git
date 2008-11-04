9c18f6e5d8f89aecbf3f1f1c414943fdb8823fd0
Seth Buntin <seth.buntin@gmail.com>
2008-11-04 20:20:45 +0000

    update get_repo utility method to handle directories that aren't valid git repo directories

--- django_git/utils.py
+++ django_git/utils.py
@@ -4,7 +4,14 @@ from git import *
 from django.conf import settings
 
 def get_repos():
-    return [Repo(os.path.join(settings.REPOS_ROOT, dir)) for dir in os.listdir(settings.REPOS_ROOT) if os.path.isdir(os.path.join(settings.REPOS_ROOT, dir))]
+    repos = []
+    for dir in os.listdir(settings.REPOS_ROOT):
+        if os.path.isdir(os.path.join(settings.REPOS_ROOT, dir)):
+            try:
+                repos.append(Repo(os.path.join(settings.REPOS_ROOT, dir)))
+            except Exception:
+                continue
+    return repos
 
 def get_repo(name):
     return Repo(os.path.join(settings.REPOS_ROOT, name))
