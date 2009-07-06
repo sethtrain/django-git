3c1a1cba7c6b8fd7a7f8dfc2e31c88cbe55bc5f6
Hugh Brown <hughdbrown@yahoo.com>
2009-07-07 01:37:47 +0800

    Refactor utils
    Signed-off-by: Seth Buntin <seth.buntin@gmail.com>

--- django_git/utils.py
+++ django_git/utils.py
@@ -4,18 +4,17 @@ from git import *
 from django.conf import settings
 
 def get_repos():
-    repos = []
-    for dir in os.listdir(settings.REPOS_ROOT):
-        if os.path.isdir(os.path.join(settings.REPOS_ROOT, dir)):
-            try:
-                repos.append(Repo(os.path.join(settings.REPOS_ROOT, dir)))
-            except Exception:
-                continue
-    return repos
+    repos = [get_repo(dir) for dir in os.listdir(settings.REPOS_ROOT)]
+    return [r for r in repos if not (r is None)]
 
 def get_repo(name):
-    repo_path = os.path.join(settings.REPOS_ROOT, dir)
-    return Repo(repo_path)
+    repo_path = os.path.join(settings.REPOS_ROOT, name)
+    if os.path.isdir(repo_path):
+        try:
+            return Repo(repo_path)
+        except Exception:
+            pass
+    return None
 
 def get_commit(name, commit):
     repo = get_repo(name)
